1541914293c425244693eb9eec29f782c38ebdea
Edward Burgess <elburgess1@gmail.com>
2016-06-30 17:15:19 -0700

    add improved player physics

--- game.js
+++ game.js
@@ -20,7 +20,6 @@ function create(){
 
   game.physics.arcade.enable(player);
 
-  player.body.bounce.y = 0.1;
   player.body.collideWorldBounds = true;
 
   cursors = game.input.keyboard.createCursorKeys();
@@ -32,13 +31,13 @@ function update() {
   player.body.velocity.x = 0;
 
   if (cursors.left.isDown){
-    player.body.velocity.x = -150;
+    player.body.velocity.x += -150;
   } else if (cursors.right.isDown){
-    player.body.velocity.x = 150;
+    player.body.velocity.x += 150;
   } else if (cursors.up.isDown) {
-    player.body.velocity.y = 150;
+    player.body.velocity.y += 150;
   } else if (cursors.down.isDown){
-    player.body.velocity.y = -150;
+    player.body.velocity.y += -150;
   } else {
     player.animations.stop();
   }
