a5628abc36db602eebae5599b035cfe082fd84dd
Edward Burgess <elburgess1@gmail.com>
2016-07-01 13:06:22 -0700

    add enemy sprite image

--- game.js
+++ game.js
@@ -5,10 +5,12 @@ var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'gameboard',       {
 
 function preload(){
   game.load.image('sky', 'assets/sky.png');
-  game.load.spritesheet('dude', 'assets/dude.png', 16, 24);
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.spritesheet('baddie', 'assets/baddie.png', 20, 32);
 }
 
 var player;
+var enemy;
 var platforms;
 
 function create(){
@@ -17,6 +19,8 @@ function create(){
   game.add.sprite(0, 0, 'sky');
 
   player = game.add.sprite(32, game.world.height - 64, 'dude');
+
+  enemy = game.add.sprite(100, game.world.height - 64, 'baddie')
 
   game.physics.arcade.enable(player);
 
@@ -35,9 +39,9 @@ function update() {
   } else if (cursors.right.isDown){
     player.body.velocity.x += 150;
   } else if (cursors.up.isDown) {
-    player.body.velocity.y += 150;
-  } else if (cursors.down.isDown){
     player.body.velocity.y += -150;
+  } else if (cursors.down.isDown){
+    player.body.velocity.y += 150;
   } else {
     player.animations.stop();
   }
